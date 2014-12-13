0584cfb14bab00e46c720b25d59be72a13359e69
Gaurav Juvekar <gauravjuvekar@gmail.com>
2014-12-13 21:57:26 +0530

    Set up mysql db and test db

--- lindsay/lindsay/settings.py
+++ lindsay/lindsay/settings.py
@@ -58,10 +58,28 @@ WSGI_APPLICATION = 'lindsay.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'djangouser',
+        'PASSWORD': 'djangouserpassword',
+        #'HOST': '127.0.0.1',
+        #'PORT': '3306',
     }
 }
+
+import sys
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'django_db',
+            'USER': 'djangotestuser',
+            'PASSWORD': 'djangotestuserpassword',
+            #'HOST': '127.0.0.1',
+            #'PORT': '3306',
+        }
+    }
+    
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
