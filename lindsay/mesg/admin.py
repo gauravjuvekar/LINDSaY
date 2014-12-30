80be68ea69519ed8e12027e5b299581159879ea0
Gaurav Juvekar <gauravjuvekar@gmail.com>
2014-12-30 23:08:24 +0530

    Remove all files

--- lindsay/mesg/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from mesg.models import Category, Message, UserDetails
-
-admin.site.register(UserDetails)
-admin.site.register(Category)
-admin.site.register(Message)
-
