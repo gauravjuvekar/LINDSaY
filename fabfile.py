23d61bf743d21b82d516b7197ed342996b8eb0bb
Gaurav Juvekar <gauravjuvekar@gmail.com>
2015-01-01 11:41:45 +0530

    Create provisions

---
+++ fabfile.py
@@ -0,0 +1,56 @@
+from fabric.api import *
+from cuisine import *
+
+def update_package_cache():
+    package_update()
+
+
+def ensure_core_packages():
+    package_list = [
+            "coreutils",
+            "python",
+            "python-pip",
+            "python-dev",
+            "build-essential",
+            "git",
+            "ufw",
+            "iptables",
+            "openssl",
+    ]
+    package_ensure(package_list)
+
+def ensure_apache():
+    package_list = [
+            "apache2",
+            "apache2-mpm-prefork",
+            "apache2-utils",
+            "libapache2-mod-wsgi",
+            "libexpat1",
+    ]
+    package_ensure(package_list)
+
+
+def ensure_mysql():
+    package_list = [
+            "mysql-server",
+            "libmysqlclient-dev",
+    ]
+    package_ensure(package_list)
+
+
+def ensure_requirements_dev_packages():
+    package_list = [
+            "libldap2-dev",
+            "libsasl2-dev",
+            "libssl-dev",
+    ]
+    package_ensure(package_list)
+
+
+@task
+def provision():
+    #execute(update_package_cache)
+    execute(ensure_core_packages)
+    execute(ensure_apache)
+    execute(ensure_mysql)
+
