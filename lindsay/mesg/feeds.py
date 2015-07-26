85698a628efada50939bddd550c7ca30f48febd9
Gaurav Juvekar <gauravjuvekar@gmail.com>
2015-07-27 01:23:43 +0530

    Update feeds

--- lindsay/mesg/feeds.py
+++ lindsay/mesg/feeds.py
@@ -9,48 +9,18 @@ from mesg.models import Category, Message
 from mesg.views import get_messages
 
 
-class DivisionFeed(Feed):
-    def get_object(self, request, division_name):
-        return get_object_or_404(Category, name=division_name, parent=None)
+class CategoryFeed(Feed):
+    def get_object(self, request, category_name):
+        return get_object_or_404(Category, url=category_name)
 
-    def title(self, division):
-        return "Feed for division %s" % division.name
+    def title(self, category):
+        return "Feed for category %s" % category.url
 
-    def link(self, division):
-        return reverse('mesg:division',
-                       kwargs={'division_name': division.name})
+    def link(self, category):
+        return reverse('mesg:category', kwargs={'category_name': category.url})
 
-    def items(self, division):
-        messages = get_messages(division,
-                                Q(expires_date__isnull=True) |
-                                Q(expires_date__gte=timezone.now())
-                                ).order_by('-pub_date')
-        return messages
-
-    def item_link(self, message):
-        return reverse('mesg:message', kwargs={'message_id': message.pk})
-
-
-class SubDivisionFeed(Feed):
-    def get_object(self, request, division_name, subdivision_name):
-        return get_object_or_404(Category, name=subdivision_name,
-                                 parent=get_object_or_404(Category,
-                                                          name=division_name,
-                                                          parent=None))
-
-    def title(self, subdivision):
-        return "Feed for division %s, subdivision %s" % (
-            subdivision.parent.name,
-            subdivision.name
-        )
-
-    def link(self, subdivision):
-        return reverse('mesg:subdivision',
-                       kwargs={'division_name': subdivision.parent.name,
-                               'subdivision_name': subdivision.name})
-
-    def items(self, subdivision):
-        messages = get_messages(subdivision,
+    def items(self, category):
+        messages = get_messages(category,
                                 Q(expires_date__isnull=True) |
                                 Q(expires_date__gte=timezone.now())
                                 ).order_by('-pub_date')
