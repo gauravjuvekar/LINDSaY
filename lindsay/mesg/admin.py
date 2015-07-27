8d6e0fd47549c507e171b17b0467df2f0a608501
Gaurav Juvekar <gauravjuvekar@gmail.com>
2015-07-27 11:37:48 +0530

    Update admin to use MPTTModelAdmin

--- lindsay/mesg/admin.py
+++ lindsay/mesg/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 from mesg.models import Category, Message, UserDetails
 
 admin.site.register(UserDetails)
-admin.site.register(Category)
+admin.site.register(Category, MPTTModelAdmin)
 admin.site.register(Message)
-
